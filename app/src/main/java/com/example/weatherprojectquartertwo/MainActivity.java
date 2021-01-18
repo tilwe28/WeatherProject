9ae2744eb45fe4191a17930ee94fd946fa6d962a
Anurag Tilwe <anurag.tilwe6@gmail.com>
2021-01-17 19:21:38 -0500

    Made layout
    Made basic layout and polished asynctask

--- app/src/main/java/com/example/weatherprojectquartertwo/MainActivity.java
+++ app/src/main/java/com/example/weatherprojectquartertwo/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.*;
 
 import org.json.JSONException;
@@ -20,68 +19,93 @@ import java.net.*;
 public class MainActivity extends AppCompatActivity {
 
     /*
-     Current weather calls
+     *Current weather calls
      *api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key} zip code
      *api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key} coordinates
-
-     Forecast calls: change weather to forecast/daily
+     *api.openweathermap.org/data/2.5/find?lat={lat}&lon={lon}&cnt={cnt}&appid={API key} circle around coordinates
+     *
+     *Forecast calls: change weather to forecast/daily
      */
 
     String apiKey = "cfa7d5a5f5ac95c413ab1ba1b48ecfe9";
-    Double lon=-74.5607, lat=40.4209;
+    Double lon, lat;//-74.5607, 40.4209;
 
-    URL url = null;
-    URLConnection urlConnection = null;
-    InputStream inputStream = null;
-    JSONObject information = new JSONObject();
+    TextView tv_currentWeather, tv_citiesInCircle, tv_cityOne, tv_cityTwo, tv_cityThree, tv_temperatureOne, tv_temperatureTwo, tv_temperatureThree, tv_timeOne, tv_timeTwo, tv_timeThree, tv_dateOne, tv_dateTwo, tv_dateThree, tv_conditionOne, tv_conditionTwo, tv_conditionThree;
+    EditText et_latitude, et_longitude;
+    ImageView iv_one, iv_two, iv_three;
+    Button button_getWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_currentWeather = findViewById(R.id.textView_currentWeather);
+        tv_citiesInCircle = findViewById(R.id.textView_citiesInCircle);
+        tv_cityOne = findViewById(R.id.textView_cityOne);
+        tv_cityTwo = findViewById(R.id.textView_cityTwo);
+        tv_cityThree = findViewById(R.id.textView_cityThree);
+        tv_temperatureOne = findViewById(R.id.textView_temperature_cityOne);
+        tv_temperatureTwo = findViewById(R.id.textView_temperature_cityTwo);
+        tv_temperatureThree = findViewById(R.id.textView_temperature_cityThree);
+        tv_timeOne = findViewById(R.id.textView_time_cityOne);
+        tv_timeTwo = findViewById(R.id.textView_time_cityTwo);
+        tv_timeThree = findViewById(R.id.textView_time_cityThree);
+        tv_dateOne = findViewById(R.id.textView_date_cityOne);
+        tv_dateTwo = findViewById(R.id.textView_date_cityTwo);
+        tv_dateThree = findViewById(R.id.textView_date_cityThree);
+        tv_conditionOne = findViewById(R.id.textView_condition_cityOne);
+        tv_conditionTwo = findViewById(R.id.textView_condition_cityTwo);
+        tv_conditionThree = findViewById(R.id.textView_condition_cityThree);
+        et_latitude = findViewById(R.id.editText_latitude);
+        et_longitude = findViewById(R.id.editText_longitutde);
+        iv_one = findViewById(R.id.imageView_cityOne);
+        iv_two = findViewById(R.id.imageView_cityTwo);
+        iv_three = findViewById(R.id.imageView_cityThree);
+        button_getWeather = findViewById(R.id.button_getWeather);
+
         URLTask urlTask = new URLTask();
         urlTask.execute();
 
     }//onCreate
 
-
-    private class URLTask extends AsyncTask<Void, Void, Void> {
+    private class URLTask extends AsyncTask<String, String, String> {
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                url = new URL("http", "api.openweathermap.org", "//data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Log.d("TAG", url.toString());
+        protected String doInBackground(String... strings) {
+
+            String json = "";
+
+            URL url = null;
+            URLConnection urlConnection = null;
+            InputStream inputStream = null;
 
             try {
+                url = new URL("http", "api.openweathermap.org", "/data/2.5/find?lat=" + lat + "&lon=" + lon + "&cnt=3"+ "&appid=" + apiKey);
                 urlConnection = url.openConnection();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //Log.d("TAG", urlConnection.toString());
-
-            try {
                 inputStream = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                int jsonChar = bufferedReader.read();
+                while (jsonChar != -1) {
+                    json += (char) jsonChar;
+                    jsonChar = bufferedReader.read();
+                }//trying to get api call from url and making it into a readable string to be used for a jsonObject
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            //Log.d("TAG", inputStream.toString());
+            }//catching any errors
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            Log.d("TAG", url.toString());
+            //Log.d("TAG", json);
 
-            try {
-                information.put("", bufferedReader.readLine());
-            } catch (JSONException | IOException e) {
-                e.printStackTrace();
-            }
-            Log.d("TAG", information.toString());
+            return json;
+        }//doInBackground
 
-            return null;
-        }
-    }
+        @Override
+        protected void onPostExecute(String s) {
 
+            //Log.d("TAG", s);
+
+        }//onPostExecute
+    }//URLTask
 }//MainActivity
