d07c55e59d364a3047c1b516169ca0ec96b671ca
Anurag Tilwe <anurag.tilwe6@gmail.com>
2021-01-19 02:20:46 -0500

    Finished onPostExecute

--- app/src/main/java/com/example/weatherprojectquartertwo/MainActivity.java
+++ app/src/main/java/com/example/weatherprojectquartertwo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weatherprojectquartertwo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
@@ -18,6 +19,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,20 +35,21 @@ public class MainActivity extends AppCompatActivity {
 
     String apiKey = "cfa7d5a5f5ac95c413ab1ba1b48ecfe9", lat, lon;//40.4209, -74.5607 Kendall Park
 
-    TextView tv_currentWeather, tv_citiesInCircle, tv_daynight, tv_cityOne, tv_cityTwo, tv_cityThree, tv_temperatureOne, tv_temperatureTwo, tv_temperatureThree, tv_timeOne, tv_timeTwo, tv_timeThree, tv_dateOne, tv_dateTwo, tv_dateThree, tv_conditionOne, tv_conditionTwo, tv_conditionThree;
+    //Declaring widgets
+    TextView tv_currentWeather, tv_citiesInCircle, tv_cityOne, tv_cityTwo, tv_cityThree, tv_temperatureOne, tv_temperatureTwo, tv_temperatureThree, tv_timeOne, tv_timeTwo, tv_timeThree, tv_dateOne, tv_dateTwo, tv_dateThree, tv_conditionOne, tv_conditionTwo, tv_conditionThree;
     EditText et_latitude, et_longitude;
+    View view_daynight, view_one, view_two, view_three;
     ImageView iv_one, iv_two, iv_three;
     Button button_getWeather;
-    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Assigning widgets
         tv_currentWeather = findViewById(R.id.textView_currentWeather);
         tv_citiesInCircle = findViewById(R.id.textView_citiesInCircle);
-        tv_daynight = findViewById(R.id.textView_daynight);
         tv_cityOne = findViewById(R.id.textView_cityOne);
         tv_cityTwo = findViewById(R.id.textView_cityTwo);
         tv_cityThree = findViewById(R.id.textView_cityThree);
@@ -68,7 +71,10 @@ public class MainActivity extends AppCompatActivity {
         iv_two = findViewById(R.id.imageView_cityTwo);
         iv_three = findViewById(R.id.imageView_cityThree);
         button_getWeather = findViewById(R.id.button_getWeather);
-        view = findViewById(R.id.view);
+        view_daynight = findViewById(R.id.view_daynight);
+        view_one = findViewById(R.id.view_ivOne);
+        view_two = findViewById(R.id.view_ivTwo);
+        view_three = findViewById(R.id.view_ivThree);
 
         iv_one.setImageResource(R.drawable.squareoutline);
         iv_two.setImageResource(R.drawable.squareoutline);
@@ -185,7 +191,44 @@ public class MainActivity extends AppCompatActivity {
                 cityTwo = information.getJSONArray("list").getJSONObject(1);
                 cityThree = information.getJSONArray("list").getJSONObject(2);
 
-                //setting the city name textview's
+                //determing time and day or night, and changing look of UI
+                //each city will be about the same so only 1 is needed to determine day or night
+                ArrayList<Integer> timesOne = timesList((cityOne.getLong("dt")));
+                if (timesOne.get(2)<7 || timesOne.get(2)>=18) {
+                    view_daynight.setBackground(getDrawable(R.drawable.background_night));
+                    tv_cityOne.setTextColor(Color.WHITE);
+                    tv_cityTwo.setTextColor(Color.WHITE);
+                    tv_cityThree.setTextColor(Color.WHITE);
+                    tv_conditionOne.setTextColor(Color.WHITE);
+                    tv_conditionTwo.setTextColor(Color.WHITE);
+                    tv_conditionThree.setTextColor(Color.WHITE);
+                    tv_temperatureOne.setTextColor(Color.WHITE);
+                    tv_temperatureTwo.setTextColor(Color.WHITE);
+                    tv_temperatureThree.setTextColor(Color.WHITE);
+                    tv_timeOne.setTextColor(Color.WHITE);
+                    tv_timeTwo.setTextColor(Color.WHITE);
+                    tv_timeThree.setTextColor(Color.WHITE);
+                    tv_dateOne.setTextColor(Color.WHITE);
+                    tv_dateTwo.setTextColor(Color.WHITE);
+                    tv_dateThree.setTextColor(Color.WHITE);
+                } else if (timesOne.get(2)>= 7 && timesOne.get(2)<18) {
+                    view_daynight.setBackground(getDrawable(R.drawable.background_day));
+                }//changes UI to day or night theme
+                //Log.d("TAG", ""+cityOne.getLong("dt"));
+
+                //setting the date textviews
+                ArrayList<Integer> timesTwo = timesList((cityTwo.getLong("dt")));
+                ArrayList<Integer> timesThree = timesList((cityThree.getLong("dt")));
+                tv_dateOne.setText(timesOne.get(0)+"/"+(timesOne.get(1)+1)+"/"+"21");
+                tv_dateTwo.setText(timesTwo.get(0)+"/"+(timesTwo.get(1)+1)+"/"+"21");
+                tv_dateThree.setText(timesThree.get(0)+"/"+(timesThree.get(1)+1)+"/"+"21");
+
+                //setting the time textviews
+                setTime(timesOne, tv_timeOne);
+                setTime(timesTwo, tv_timeTwo);
+                setTime(timesThree, tv_timeThree);
+
+                //setting the city name textviews
                 tv_cityOne.setText(cityOne.getString("name"));
                 tv_cityTwo.setText(cityTwo.getString("name"));
                 tv_cityThree.setText(cityThree.getString("name"));
@@ -203,23 +246,14 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject conditionTwo = cityTwo.getJSONArray("weather").getJSONObject(0);
                 JSONObject conditionThree = cityThree.getJSONArray("weather").getJSONObject(0);
                 setCondition(conditionOne, iv_one, tv_conditionOne);
-                setCondition(conditionOne, iv_two, tv_conditionTwo);
-                setCondition(conditionOne, iv_three, tv_conditionThree);
-
-                /*
-                long epochTime = cityOne.getLong("dt");
-                Date date = new Date(epochTime*1000L);
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
-                String formattedDate = dateFormat.format(date);
-                Log.d("TAG", ""+epochTime);
-                Log.d("TAG", formattedDate);
-                 */
+                setCondition(conditionTwo, iv_two, tv_conditionTwo);
+                setCondition(conditionThree, iv_three, tv_conditionThree);
 
             }//trying to display weather information to the ui
             catch (JSONException e) {
                 e.printStackTrace();
             }//catches any errors
-            Log.d("TAG", information.toString());
+            //Log.d("TAG", information.toString());
             Log.d("TAG", cityOne.toString());
             Log.d("TAG", cityTwo.toString());
             Log.d("TAG", cityThree.toString());
@@ -256,5 +290,45 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }//setCondition
+
+    public ArrayList<Integer> timesList(long epochTime) {
+
+        ArrayList<Integer> times = new ArrayList<>();
+
+        //this time isn't the current time but the time that correlates to when this weather data is for (it will be very recent)
+        epochTime = epochTime - 1609477200;//seconds since the start of 2021 in EST
+        int days = (int) (epochTime/86400);//days that have passed (it should be minus 1 of the date like if it it's Jan 19, then 18 days passed)
+        int seconds = (int) (epochTime - (days*86400));//seconds that have passed in the current day
+        int hour = (seconds/3600);//hours that have passed in current day
+        seconds = seconds - (hour*3600);//seconds within the hour
+        int minutes = seconds/60;//minutes in hour
+        int month = ((days+1)/31)+1;//month of the year
+        Log.d("TAG", ""+epochTime);
+        Log.d("TAG", ""+month);
+        Log.d("TAG", ""+days);
+        Log.d("TAG", ""+hour);
+        Log.d("TAG", ""+minutes);
+        Log.d("TAG", ""+seconds);
+
+        times.add(0, month);
+        times.add(1, days);
+        times.add(2, hour);
+        times.add(3, minutes);
+        times.add(4, seconds);
+
+        return times;
+    }//cityTime
+
+    public void setTime(ArrayList<Integer> times, TextView tv) {
+        String ampm = "AM";
+        if (times.get(2)>=12) {
+            ampm = "PM";
+            if (times.get(2)>12)
+                times.set(2, times.get(2)-12);
+        }
+        if (times.get(3)<10)
+            tv.setText(times.get(2)+":0"+times.get(3)+" "+ampm+" EST");
+        else tv.setText(times.get(2)+":"+times.get(3)+" "+ampm+" EST");
+    }//setTime
 
 }//MainActivity
